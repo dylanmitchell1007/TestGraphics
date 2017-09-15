bbe15fbd3197ecafe8900933fd45954efa3d3f96
dylanmitchell1007 <armadummy@yahoo.com>
2017-09-15 16:53:18 -0500

    setup for assessment. does not render

--- Graphics/year2/3.RenderingGeometry/RenderingGeometryApp.cpp
+++ Graphics/year2/3.RenderingGeometry/RenderingGeometryApp.cpp
@@ -1,265 +1,24 @@
-#define GLM_FORCE_SWIZZLE
 
 #include "RenderingGeometryApp.h"
-#include "gl_core_4_4.h"
-#include <GLFW/glfw3.h>
-
+#include <gl_core_4_4.h>
+#include <GLFW\glfw3.h>
+#include <glm\glm.hpp>
+#include <vector>
+#include <Camera.h>
+#include "Mesh.h"
+#include "Shader.h"
 #include <imgui.h>
 #include <imgui_impl_glfw_gl3.h>
-#include <glm/fwd.hpp>
-#include <glm/common.hpp>
-#include <glm/ext.hpp>
-#include <glm/mat4x4.hpp>
-
-#include <Transform.h>
-#include <Camera.h>
-#include "Shader.h"
-#include <iostream>
-
-
-RenderingGeometryApp::RenderingGeometryApp() : m_runningTime(0), m_camera(nullptr), m_mouseX(0),
-m_mouseY(0),
-m_mouseButtonDown(false),
-m_mouseButtonUp(false), m_isDragging(false), m_deltaX(0), m_deltaY(0), m_startX(0),
-m_startY(0), m_endX(0),
-m_endY(0),
-m_angleAxis(true),
-perspective(true)
+#include <Gizmos.h>
+ 
+RenderingGeometryApp::RenderingGeometryApp()
 {
-}
-
-
+};
 RenderingGeometryApp::~RenderingGeometryApp()
 {
+
 }
 
-
-void RenderingGeometryApp::startup()
-{
-	//setup camera info
-	m_fov = glm::quarter_pi<float>();
-	m_aspectRatio = 16 / 9.f;
-	m_near = .1f;
-	m_far = 1000.f;
-	m_top = 50;
-	m_bottom = -50;
-	m_left = -50;
-	m_right = 50;
-
-	const glm::vec3 eye = glm::vec3(10, 10, 10);//camera in world is at positive 10
-	const glm::vec3 target = glm::vec3(0);
-	const glm::vec3 up = glm::vec3(0, 1, 0);
-
-	m_camera = new Camera();
-	m_camera->setPerspective(m_fov, m_aspectRatio, m_near, m_far);
-	m_camera->setLookAt(eye, target, up);
-
-	setKeyCallback(key_callback);
-	setScrollCallback(scroll_callback);
-
-	Vertex a = { glm::vec4(-0.5,  0, 0, 1)		, glm::vec4(1, 0, 0, .5) };
-	Vertex b = { glm::vec4(0.5,  0, 0, 1)			, glm::vec4(0, 1, 0, .5) };
-	Vertex c = { glm::vec4(0.5, -0.5, 0, 1)			, glm::vec4(0, 0, 1, .5) };
-	Vertex d = { glm::vec4(-0.5, -0.5, 0, 1)		, glm::vec4(1, 0, 0, .5) };
-	Vertex e = { glm::vec4(-0.5,  0.5, 0, 1)		, glm::vec4(0, 1, 0, .5) };
-
-	Vertex f = { glm::vec4(0, 0, 0, 1)		, glm::vec4(1, 0, 0, .5) };
-	Vertex g = { glm::vec4(5,  0, 0, 1)		, glm::vec4(1, 0, 0, .5) };
-	Vertex h = { glm::vec4(5,  0, 5, 1)		, glm::vec4(1, 0, 0, .5) };
-	Vertex i = { glm::vec4(0,  0, 5, 1)		, glm::vec4(1, 0, 0, .5) };
-
-
-	std::vector<Vertex> myPlane = { f, g, h, i };
-	std::vector<unsigned int> planeIndices = { 0,1,2,3 };
-
-	std::vector<Vertex> myObject = { a, b, c, d, e };
-	std::vector<unsigned int> objectIndices = { 0, 1, 2, 0, 2, 3, 0, 4, 1 };
-	//create vertex array
-	/*this->object = new Mesh();
-	this->object->initialize(myObject, objectIndices);*/
-	this->object = new Mesh();
-	this->object->initialize(myPlane, planeIndices);
-	//create buffers
-	this->object->create_buffers();
-	//buffer data
-
-	//setup vertex attributes
-
-	//build shader
-	this->myshader = new Shader();
-	this->myshader->defaultLoad();
-}
-
-void RenderingGeometryApp::update(float delta_time)
-{
-	m_runningTime += delta_time;
-	m_deltaTime = delta_time;
-	m_mouseButtonDown = glfwGetMouseButton(m_window, GLFW_MOUSE_BUTTON_2) == GLFW_PRESS;
-	m_mouseButtonUp = glfwGetMouseButton(m_window, GLFW_MOUSE_BUTTON_2) == GLFW_RELEASE;
-
-	glfwGetCursorPos(m_window, &m_mouseX, &m_mouseY);
-	//if clicked
-	if (m_mouseButtonUp)
-	{
-		m_isDragging = false;
-		m_deltaX = m_deltaY = 0;
-		m_startX = m_startY = 0;
-		m_endX = m_endY = 0;
-	}
-
-	if (m_mouseButtonDown)
-	{
-		if (m_isDragging == false)
-		{
-			m_isDragging = true;
-			glfwGetCursorPos(m_window, &m_startX, &m_startY);
-		}
-
-		m_deltaX = (m_mouseX - m_startX);
-		m_deltaY = (m_mouseY - m_startY);
-	}
-
-	static int width, height;
-
-	glfwGetWindowSize(m_window, &width, &height);
-
-	if (m_deltaY != 0)
-	{
-		float angle = (float)(m_deltaY / height) * delta_time;
-
-		if (m_angleAxis)
-			m_camera->transform.rotateAround(-angle, m_camera->transform.right.xyz());
-		else
-			m_camera->transform.rotate(angle, m_camera->transform.right.xyz());
-	}
-
-	if (m_deltaX != 0)
-	{
-		float angle = (float)(m_deltaX / width) * delta_time;
-
-		if (m_angleAxis)
-			m_camera->transform.rotateAround(-angle, glm::vec3(0, 1, 0));
-		else
-			m_camera->transform.rotate(angle, glm::vec3(0, 1, 0));
-	}
-
-
-	if (glfwGetKey(m_window, 'W'))
-	{
-		//m_camera->transform.translate(glm::vec3(0, 0, -10));
-
-		auto V = m_camera->view;
-		auto T = glm::mat4(
-			glm::vec4(1, 0, 0, 0),
-			glm::vec4(0, 1, 0, 0),
-			glm::vec4(0, 0, 1, 0),
-			glm::vec4(0, 0, -10, 1)
-		);
-
-		auto view = V * T;
-		auto world = glm::inverse(view);
-		m_camera->transform.setWorld(world);
-	}
-		
-	if (glfwGetKey(m_window, 'S'))
-	{
-		//m_camera->transform.translate(glm::vec3(0, 0, 10));
-		auto V = m_camera->view;
-		auto T = glm::mat4(
-			glm::vec4(1, 0, 0, 0),
-			glm::vec4(0, 1, 0, 0),
-			glm::vec4(0, 0, 1, 0),
-			glm::vec4(0, 0, 10, 1)
-		);
-
-		auto view = V * T;
-		auto world = glm::inverse(view);
-		m_camera->transform.setWorld(world);
-	}
-		
-	if (glfwGetKey(m_window, 'A'))
-	{
-		//m_camera->transform.translate(glm::vec3(-10, 0, 0));
-		auto V = m_camera->view;
-		auto T = glm::mat4(
-			glm::vec4(1, 0, 0, 0),
-			glm::vec4(0, 1, 0, 0),
-			glm::vec4(0, 0, 1, 0),
-			glm::vec4(-10, 0, 0, 1)
-		);
-
-		auto view = V * T;
-		auto world = glm::inverse(view);
-		m_camera->transform.setWorld(world);
-	}
-		
-	if (glfwGetKey(m_window, 'D'))
-	{
-		//m_camera->transform.translate(glm::vec3(10, 0, 0));
-		auto V = m_camera->view;
-		auto T = glm::mat4(
-			glm::vec4(1, 0, 0, 0),
-			glm::vec4(0, 1, 0, 0),
-			glm::vec4(0, 0, 1, 0),
-			glm::vec4(10, 0, 0, 1)
-		);
-
-		auto view = V * T;
-		auto world = glm::inverse(view);
-		m_camera->transform.setWorld(world);
-	}	
-
-	if (glfwGetKey(m_window, 'F'))
-	{
-		//m_camera->setLookAt(m_camera->transform.World[3].xyz(), glm::vec3(0), glm::vec3(0, 1, 0));
-	}
-
-	(perspective) ?
-		m_camera->setPerspective(m_fov, m_aspectRatio, m_near, m_far) :
-		m_camera->setOrthographic(m_left, m_right, m_bottom, m_top, m_near, m_far);
-
-	m_camera->update(delta_time);
-
-	auto camPos = m_camera->view[3];
-	
-	system("cls");
-	std::cout << camPos.x << "/" << camPos.y << "/" << camPos.z;
-}
-glm::mat4 aPlane = glm::mat4(2);
-glm::mat4 aObject = glm::mat4(1);
-void RenderingGeometryApp::draw()
-{
-	//draw object
-	this->myshader->bind();
-
-	auto translation = glm::mat4(
-		glm::vec4(1, 0, 0, 0),
-		glm::vec4(0, 1, 0, 0),
-		glm::vec4(0, 0, 1, 0),
-		glm::vec4(10, 10, 10, 1)
-	
-	);
-	//aObject = aObject * translation;
-	unsigned int projectionViewUniform = this->myshader->getUniform("ProjectionViewWorld");
-	glUniformMatrix4fv(projectionViewUniform, 1, GL_FALSE, glm::value_ptr(m_camera->projectionView * aObject));
-	this->object->draw(GL_QUADS);
-	this->myshader->unbind();
-
-#pragma region UI
-
-	ImGui_ImplGlfwGL3_NewFrame();
-	ImGui::Begin("Menu", (bool*)0, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
-	m_gameover = ImGui::Button("Quit?", ImVec2(100, 50));
-	ImGui::End();
-
-
-	ImGui::Begin("Rendering Options 2", (bool*)0, ImGuiWindowFlags_NoResize);
-	ImGui::Text("Running Time: %f", m_runningTime);
-	ImGui::Text("delta time : %f", m_deltaTime);
-	ImGui::End();
-
-#pragma endregion UI
-}
 std::vector<glm::vec3> halfcircle(float radius, unsigned int points)
 {
 	std::vector<glm::vec3> tmpArray;
@@ -273,33 +32,172 @@ std::vector<glm::vec3> halfcircle(float radius, unsigned int points)
 	}
 	return tmpArray;
 }
+void generateCube(Mesh* cube)
+{	
+	//front Bottom Left
+	Vertex j = { glm::vec4(0.0f, 0.0f, 0, 1.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f) };
+	//front Bottom Right
+	Vertex k = { glm::vec4(1.0f, 0.0f, 0, 1.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f) };
+	//front Top Right
+	Vertex l = { glm::vec4(1.0f, 1.0f, 0, 1.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f) };
+	//front Top Left
+	Vertex m = { glm::vec4(0.0f, 1.0f, 0, 1.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f) };
+	//Back Bottom Left
+	Vertex n = { glm::vec4(0, 0.0f, 1, 1.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f) };
+	//Back Bottom Right
+	Vertex o = { glm::vec4(1.0f, 0.0f, 1, 1.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f) };
+	//Back Top Right
+	Vertex p = { glm::vec4(1.0f, 1.0f, 1, 1.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f) };
+	//Back Top Left
+	Vertex q = { glm::vec4(0.0f, 1.0f, 1, 1.0f), glm::vec4(1.0f, 0.0f, 0.0f, 1.0f) };
+
+	std::vector<Vertex> Cubevert = { j, k, l, m, n, o, p, q };
+	std::vector<unsigned int> Cubeindices = {
+		0,  1,  3,  3,  1,  2,   //frontface
+		4,  5,  7, 7,  5,  6,  //backface
+		4, 0, 7, 7, 0, 3,  //leftface
+		1,  5,  2,  2, 5,  6, //rightface
+		3, 2, 7, 7, 2, 6,  //topface
+		0, 1, 4, 4, 1, 5 }; //bottom face
+	cube->initialize(Cubevert, Cubeindices);	
+}
+void generatePlane(Mesh* plane)
+{		
+	//bottomleft
+	Vertex f = { glm::vec4(0, 0, 0, 1)		, glm::vec4(0.5, 0, 0, .5) };
+	//bottomright
+	Vertex g = { glm::vec4(5,  0, 0, 1)		, glm::vec4(0.5, 0, 0, .5) };
+	//topright
+	Vertex h = { glm::vec4(5,  0, 5, 1)		, glm::vec4(0.5, 0, 0, .5) };
+	//topleft
+	Vertex i = { glm::vec4(0,  0, 5, 1)		, glm::vec4(0.5, 0, 0, .5) };
+
+	//Plane Vertex
+	std::vector<Vertex> myPlane = { f, g, h, i };
+	//Plane Indices
+	std::vector<unsigned int> planeIndices = { 0,1,3,3,2,1 };
+	plane->initialize(myPlane, planeIndices);
+	
+}
+ //ToDo: Generate Sphere
+
+void RenderingGeometryApp::startup()
+{
+
+	const glm::vec3 eye = glm::vec3(10, 10, -100);//camera in world is at positive 10
+	const glm::vec3 target = glm::vec3(0);
+	const glm::vec3 up = glm::vec3(0, 1, 0);
+
+	m_camera = new Camera();
+	/*m_camera->setPerspective(m_fov, m_aspectRatio, m_near, m_far);*/
+	m_camera->setLookAt(eye, target, up);
+
+
+	Vertex a = { glm::vec4(-0.5,  0, 0, 1)		, glm::vec4(1, 0, 0, .5) };
+	Vertex b = { glm::vec4(0.5,  0, 0, 1)			, glm::vec4(0, 1, 0, .5) };
+	Vertex c = { glm::vec4(0.5, -0.5, 0, 1)			, glm::vec4(0, 0, 1, .5) };
+	Vertex d = { glm::vec4(-0.5, -0.5, 0, 1)		, glm::vec4(1, 0, 0, .5) };
+	Vertex e = { glm::vec4(-0.5,  0.5, 0, 1)		, glm::vec4(0, 1, 0, .5) };
+
+
+
+	//Face of Cube
+
+
+
+	//Cube Indices
+
+
+
+	std::vector<Vertex> myObject = { a, b, c, d, e };
+
+	std::vector<unsigned int> objectIndices = { 0, 1, 2, 0, 2, 3, 0, 4, 1 };
+
+
+	//create vertex array
+	this->cube_Mesh = new Mesh();	
+	generateCube(this->cube_Mesh);
+	this->cube_Mesh->create_buffers();
+	
+	this->plane_Mesh = new Mesh();
+	generatePlane(this->plane_Mesh);
+	this->plane_Mesh->create_buffers();
+	
+	
+	
+	//create buffers
+	
+	//buffer data
+
+	//setup vertex attributes
+
+	//build shader
+	this->myshader = new Shader();
+	this->myshader->defaultLoad();
+	Gizmos::create();
+}
+
+void RenderingGeometryApp::update(float delta_time)
+{/*
+	if (glfwGetKey(m_window, 'W') == GLFW_PRESS)
+	{
+		m_camera->setPosition(m_camera->getWorldTransform()[3] -= m_camera->getWorldTransform()[1]);
+	}
+	if (glfwGetKey(_window, 'D') == GLFW_PRESS)
+	{
+		m_camera->setPosition(m_camera->getWorldTransform()[3] += m_camera->getWorldTransform()[0]);
+	}
+	if (glfwGetKey(_window, 'A') == GLFW_PRESS)
+	{
+		m_camera->setPosition(m_camera->getWorldTransform()[3] -= m_camera->getWorldTransform()[0]);
+	}
+	if (glfwGetKey(_window, 'Q') == GLFW_PRESS)
+	{
+		m_camera->setPosition(m_camera->getWorldTransform()[3] += m_camera->getWorldTransform()[1]);
+	}
+	if (glfwGetKey(_window, 'E') == GLFW_PRESS)
+	{
+		m_camera->setPosition(m_camera->getWorldTransform()[3] -= m_camera->getWorldTransform()[1]);
+	}*/
+	/*if (glfwGetKey(_window, 'F') == GLFW_PRESS)
+	{
+	m_camera->setLookAt(m_camera->setPosition(), vec3(0, 0, 0), vec3(0, 1, 0));*/
+	//m_runningTime += delta_time;
+	//deltaTime = delta_time;
+
+	//(perspective) ?
+	//	m_camera->setPerspective(m_fov, m_aspectRatio, m_near, m_far) :
+	//	m_camera->setOrtho(m_left, m_right, m_bottom, m_top, m_near, m_far);
+	ImGui_ImplGlfwGL3_NewFrame();
+	ImGui::Begin("hello");
+	ImGui::End();
+	m_camera->update(delta_time);
+	
+}
+
+void RenderingGeometryApp::draw()
+{
+	//draw object
+	this->myshader->bind();
+
+	//aObject = aObject * translation;
+	auto projectionViewUniform = this->myshader->getUniform("ProjectionViewWorld");
+	auto plane_ModelMatrix = glm::mat4(1);
+	
+	glUniformMatrix4fv(projectionViewUniform, 1, GL_FALSE, glm::value_ptr(m_camera->projectionView));
+	this->cube_Mesh->draw(GL_TRIANGLES);
+
+
+	projectionViewUniform = this->myshader->getUniform("ProjectionViewWorld");
+	auto cube_ModelMatrix = glm::mat4(1) * glm::translate(glm::vec3(5, 0, 0)) * glm::scale(glm::vec3(15));
+	auto mvp = m_camera->projectionView * cube_ModelMatrix;
+	glUniformMatrix4fv(projectionViewUniform, 1, GL_FALSE, glm::value_ptr(mvp));
+	this->plane_Mesh->draw(GL_TRIANGLES);
+	this->myshader->unbind();
+
+
+}
+
 void RenderingGeometryApp::shutdown()
 {
-}
-
-void RenderingGeometryApp::setKeyCallback(KeyFunc func)
-{
-	glfwSetWindowUserPointer(m_window, this);
-	glfwSetKeyCallback(m_window, func);
-}
-
-void RenderingGeometryApp::setScrollCallback(ScrollFunc func)
-{
-	glfwSetWindowUserPointer(m_window, this);
-	glfwSetScrollCallback(m_window, func);
-}
-
-void RenderingGeometryApp::scroll_callback(GLFWwindow * window, double xoffset, double yoffset)
-{
-	RenderingGeometryApp* instance = (RenderingGeometryApp*)glfwGetWindowUserPointer(window);
-	instance->m_camera->transform.translate(glm::vec3(0, -yoffset, 0));
-}
-
-void RenderingGeometryApp::key_callback(GLFWwindow * window, int key, int scancode, int action, int mods)
-{
-	RenderingGeometryApp* instance = (RenderingGeometryApp*)glfwGetWindowUserPointer(window);
-	Camera* camera = instance->m_camera;
-	if (key == GLFW_KEY_SPACE)
-		camera->setLookAt(glm::vec3(10, 10, -10), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
-
 }
--- Graphics/year2/3.RenderingGeometry/RenderingGeometryApp.h
+++ Graphics/year2/3.RenderingGeometry/RenderingGeometryApp.h
@@ -1,16 +1,11 @@
 #pragma once
-#include "Shader.h"
-#include "Mesh.h"
-#include "Application.h"
-#include <glm/glm.hpp>
+#include <Application.h>
 
-
-typedef void(*KeyFunc)(GLFWwindow *window, int key, int scancode, int action, int mods);
-typedef void(*ScrollFunc)(GLFWwindow* window, double xoffset, double yoffset);
-class Transform;
+//forward declare to reduce dependencies
+class Shader;
+class Mesh;
+class Texture;
 class Camera;
-
-
 class RenderingGeometryApp : public Application
 {
 public:
@@ -19,49 +14,19 @@ public:
 
 	// Inherited via Application
 	virtual void startup() override;
+
 	virtual void shutdown() override;
-	virtual void update(float) override;
+
+	virtual void update(float time) override;
+
 	virtual void draw() override;
 
-	static void scroll_callback(GLFWwindow * window, double xoffset, double yoffset);
-	static void key_callback(GLFWwindow * window, int key, int scancode, int action, int mods);
-protected:
-	void setKeyCallback(KeyFunc func);
-	void setScrollCallback(ScrollFunc func);
-
-
 private:
-	
-	bool perspective;
-	bool m_angleAxis;
 
-	float m_runningTime;
-
+	Mesh* cube_Mesh; //plane
+	Mesh* plane_Mesh; //square or cube
+	Mesh* loadOBJ;
 	Shader* myshader;
-	Mesh* object;
+	Shader* triShader;
 	Camera* m_camera;
-	Transform* m_earth;
-	Transform* m_sun;
-	float m_deltaTime;
-	double m_mouseX;
-	double m_mouseY;
-	bool m_mouseButtonDown;
-	bool m_mouseButtonUp;
-	bool m_isDragging;
-	float m_deltaX;
-	float m_deltaY;
-	double m_startX;
-	double m_startY;
-	double m_endX;
-	double m_endY;
-	float m_fov;
-	float m_aspectRatio;
-	float m_near;
-	float m_far;
-	float m_top;
-	float m_bottom;
-	float m_left;
-	float m_right;
-
 };
-
